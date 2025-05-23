042c59b3ca519ffbd4becf2d4f69e3f5d91d8d98
Deepak Porwal <deepak.porwal@capgemini.com>
2025-05-23 11:47:17 +0530

    initial project structure

---
+++ setup.py
@@ -0,0 +1,11 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name="Zomato chatbot project",
+    version="0.0.1",
+    author="Deepak",
+    author_email="dporwal04@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
